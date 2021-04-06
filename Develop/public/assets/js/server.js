8783cdfbd147266e77df38e0a9ff0145d858a80a
HRush <hrkoren@gmail.com>
2021-04-05 21:14:19 -0400

    add more code to server.js

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -9,3 +9,34 @@ const PORT = process.env.PORT || 3000;
 //set up express to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+//notes data
+
+const activeNote = [
+    {
+        noteTitle: '',
+        noteText: '',
+    }
+]
+
+const newNote = [
+    {
+        noteId: '',
+        title: '',
+        text: '',
+    }
+]
+
+//routes
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+
+app.get('/notes', (req,res) => res.sendFile(path.join(__dirname, 'notes.html')));
+
+//display all notes
+app.get('/api/notes', (req, res) => res.json(notes));
+
+//display a single note, or return false
+app.get('api/notes/:note', (req, res) => {
+    const searched = req.params.note;
+    console.log(searched);
+})
